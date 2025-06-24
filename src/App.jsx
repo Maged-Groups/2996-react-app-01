cc44b5fb712d86a2d8210692529e67e359558264
Maged <magedyaseengroups@gmail.com>
2025-06-24 22:19:05 +0300

    Install BS5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='border border-5 border-danger p-4 d-flex justify-content-center gap-5'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
